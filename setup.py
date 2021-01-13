f93452dd8acdc205c6cf1c399ff0a0fa856931eb
ghorbani <m.ghorbani2357@gmail.com>
2021-01-13 19:18:55 +0330

    Initial commit

--- setup.py
+++ setup.py
@@ -12,21 +12,17 @@ import versioneer
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 
-REQUIREMENTS = filter(None, open(
-    os.path.join(this_dir, 'requirements', 'main.txt')).read().splitlines())
+REQUIREMENTS = filter(None, open('requirements.txt').read().splitlines())
 
 setup(
     name='Function Limiter',
     author=__author__,
     author_email=__email__,
     license="MIT",
-    url="https://flask-limiter.readthedocs.org",
     zip_safe=False,
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     install_requires=list(REQUIREMENTS),
-    classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
+    classifiers=[k for k in open('').read().split('\n') if k],
     description='Rate limiting for callable functions',
-    long_description=open('README.rst').read() + open('HISTORY.rst').read(),
-    packages=find_packages(exclude=["tests*"]),
 )
